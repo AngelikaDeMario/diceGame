7e058aecfd57ccfeea8f9b3467b5337361ca60af
Angelika DeMario <angelikademario@gmail.com>
2017-10-31 14:08:42 -0500

    move files to the github

---
+++ dicegame.js
@@ -0,0 +1,14 @@
+var die1 = (Math.floor(Math.random)*6+1);
+var die2 = (Math.floor(Math.random)*6+1);
+var score = (die1, die2)
+
+if (die1===1 || die2===2){
+	console.log(score = 0);
+} else {
+	console.log (die1 = die2);
+}
+if (die1 && die2 ===1){
+	console.log(score = 0)
+} else {
+	console.log(score = 2*(die1 + die2));
+}
