051be9ab010434be5f52deaf957feaa38b050d3c
Angelika DeMario <angelikademario@gmail.com>
2017-11-01 15:10:14 -0500

    move all files to the github

--- dicegame.js
+++ dicegame.js
@@ -1,14 +1,238 @@
-var die1 = (Math.floor(Math.random)*6+1);
-var die2 = (Math.floor(Math.random)*6+1);
-var score = (die1, die2)
+var data [
+{	
+	question: "What House was Harry Potter sorted into?", 
+	answer: "Gryffindor",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{	
+	question "Who is Harry Potter's Slytherin enemy?",
+	answer: "Draco Malfoy",
+	difficulty: 1,
+	topic: "Hogwarts Level 1"
+}
+{
+	question: "What is the name of Harry's owl?",
+	answer: "Hedwig",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "Where does Harry sleep at his Aunt and Uncle's house in the first book?",
+	answer: "under the stairs",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{ 	
+	question: "Who killed Harry Potter's parents?",
+	answer: "Voldemort",
+	difficulty: 1
+	topic: "Hogwarts Level 1",
+}
+{ 
+	question: "What color is the Weasley's hair?",
+	answer: "red",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What does the Wizarding World refer to humans by?",
+	answer: "Muggles",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "Which animal delivers mail?",
+	answer: "owl",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What is the Wizarding bank called?",
+	answer: "Gringotts",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What creatures run the Wizarding bank?",
+	answer: "goblins",
+	difficulty: 1,
+	topic: "Hogwarts Level 1", 
+}
+{
+	question: "Who doese Harry Potter marry?",
+	answer: "Ginny Weasley",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What was the name of the house elf that Harry set free?",
+	answer: "Dobby",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What object decides what House students get sorted into?",
+	answer: "The Sorting Hat",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What is the official sport at Hogwarts?",
+	answer: "Quidditch",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "Who did Hermione Granger marry?",
+	ansewr: "Ron Weasley",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "Who wrote Harry Potter?",
+	answer: "J.K. Rowling",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{ 
+	question: "Who is Harry Potter's ultimate enemy?",
+	answer: "Voldemort",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What is Harry's last name?",
+	answer: "Potter",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{
+	question: "What do quidditch players ride?",
+	answer: "broomsticks",
+	difficulty: 1,
+	topic: "Hogwarts Level 1",
+}
+{ 
+	question: "What type of creature did Harry have to fight in the Goblet of Fire?";
+	answer: "dragon",
+	difficulty: 1, 
+	topic: "Hogwarts Level 1",
+}
 
-if (die1===1 || die2===2){
-	console.log(score = 0);
-} else {
-	console.log (die1 = die2);
-}
-if (die1 && die2 ===1){
-	console.log(score = 0)
-} else {
-	console.log(score = 2*(die1 + die2));
-}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
