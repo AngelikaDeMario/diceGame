38e68c26c24bb0b1e2ad6d4eccc7c5293dd4f733
Angelika DeMario <angelikademario@gmail.com>
2017-11-03 08:26:42 -0500

    move files to the github

--- app.js
+++ app.js
@@ -1,2 +1,150 @@
-function (displayquestion) {
+
+// displayQuestions(data);
+let questionResult = function pickQuestion(diceRoll){
+if (obj.difficulty=1)(function
+return myQuestion.question[diceRoll]
+}
+
+
+function roundOne(myQuestions){
+	var rolledNumber = rollDice(20);
+	var questionResult = getQuestion(rolledNumber, myQuestions);
+	return questionResult;
+}
+
+function rollDice(diceSide){
+	var rollNumber = Math.floor(Math.random() * diceSide + 1); 
+	return rollNumber;
+}
+
+function getQuestion(rolledNumber, myQuestions){
+	var results = findDifficultyQuestions(myQuestions, 1);
+	var question = findRoundOneQuestion(results, rolledNumber);
+	return question;
+}
+
+function findDifficultyQuestions(myQuestions, difficultyLevel) {
+	 var output = [];
+		
+	 for (var i = 0; i < myQuestions.length; i++) {
+		 if (myQuestions[i].difficulty === difficultyLevel) {
+
+			output.push(myQuestions[i]);
+		 }
+	 }
+	 return output;
+}
+
+function findRoundOneQuestion(results, rolledNumber){
+	//switch case for only round one question options. in each case have a return results.question;
+	switch(rolledNumber) {
+		case 1:
+		return results[0].question;
+		case 2:
+		return results[1].question;
+		case 3:
+		return results[2].question;
+		case 4:
+		return results[3].question;
+		case 5:
+		return results[4].question;
+		case 6:
+		return results[5].question;
+		case 7:
+		return results[6].question;
+		case 8:
+		return results[7].question;
+		case 9:
+		return results[8].question;
+		case 10:
+		return results[9].question;
+		case 11:
+		return results[10].question;
+		case 12:
+		return results[11].question;
+		case 13:
+		return results[12].question;
+		case 14:
+		return results[13].question;
+		case 15:
+		return results[14].question;
+		case 16:
+		return results[15].question;
+		case 17:
+		return results[16].question;
+		case 18:
+		return results[17].question;
+		case 19:
+		return results[18].question;
+		case 20:
+		return results[19].question;
+		default 21: 
+		return results[20].questions;
+	}
+}
+
+// displayQuestions(data);
+let questionResult = function pickQuestion(diceRoll){
+if (obj.difficulty=2)(function
+return myQuestion.question[diceRoll]
+}
+
+
+function roundOne(myQuestions){
+	var rolledNumber = rollDice(12);
+	var questionResult = getQuestion(rolledNumber, myQuestions);
+	return questionResult;
+}
+
+function rollDice(diceSide){
+	var rollNumber = Math.floor(Math.random() * diceSide + 1); 
+	return rollNumber;
+}
+
+function getQuestion(rolledNumber, myQuestions){
+	var results = findDifficultyQuestions(myQuestions, 1);
+	var question = findRoundOneQuestion(results, rolledNumber);
+	return question;
+}
+
+function findDifficultyQuestions(myQuestions, difficultyLevel) {
+	 var output = [];
+		
+	 for (var i = 0; i < myQuestions.length; i++) {
+		 if (myQuestions[i].difficulty === difficultyLevel) {
+
+			output.push(myQuestions[i]);
+		 }
+	 }
+	 return output;
+}
+
+function findRoundOneQuestion(results, rolledNumber){
+	case 1: 
+	return results[21].question;
+	case 2: 
+	return results[22].question;
+	case 3: 
+	return results[23].question;
+	case 4: 
+	return results[24].question;
+	case 5: 
+	return results[25].question;
+	case 6: 
+	return results[26].question;
+	case 7: 
+	return results[27].question;
+	case 8: 
+	return results[28].question;
+	case 9: 
+	return results[29].question;
+	case 10: 
+	return results[30].question;
+	case 11: 
+	return results[31].question;
+	case 12: 
+	return results[32].question;
+	default 13:
+	return results[33].quetion;
+}
 	
--- dicegame.js
+++ dicegame.js
@@ -1,124 +1,373 @@
-var data [
+const myQuestions = [
 {	
 	question: "What House was Harry Potter sorted into?", 
 	answer: "Gryffindor",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {	
 	question "Who is Harry Potter's Slytherin enemy?",
 	answer: "Draco Malfoy",
 	difficulty: 1,
 	topic: "Hogwarts Level 1"
-}
+},
 {
 	question: "What is the name of Harry's owl?",
 	answer: "Hedwig",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "Where does Harry sleep at his Aunt and Uncle's house in the first book?",
 	answer: "under the stairs",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 { 	
 	question: "Who killed Harry Potter's parents?",
 	answer: "Voldemort",
 	difficulty: 1
 	topic: "Hogwarts Level 1",
-}
+},
 { 
 	question: "What color is the Weasley's hair?",
 	answer: "red",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What does the Wizarding World refer to humans by?",
 	answer: "Muggles",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "Which animal delivers mail?",
 	answer: "owl",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What is the Wizarding bank called?",
 	answer: "Gringotts",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What creatures run the Wizarding bank?",
 	answer: "goblins",
 	difficulty: 1,
 	topic: "Hogwarts Level 1", 
-}
+},
 {
 	question: "Who doese Harry Potter marry?",
 	answer: "Ginny Weasley",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What was the name of the house elf that Harry set free?",
 	answer: "Dobby",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What object decides what House students get sorted into?",
 	answer: "The Sorting Hat",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What is the official sport at Hogwarts?",
 	answer: "Quidditch",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "Who did Hermione Granger marry?",
 	ansewr: "Ron Weasley",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "Who wrote Harry Potter?",
 	answer: "J.K. Rowling",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 { 
 	question: "Who is Harry Potter's ultimate enemy?",
 	answer: "Voldemort",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What is Harry's last name?",
 	answer: "Potter",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 {
 	question: "What do quidditch players ride?",
 	answer: "broomsticks",
 	difficulty: 1,
 	topic: "Hogwarts Level 1",
-}
+},
 { 
-	question: "What type of creature did Harry have to fight in the Goblet of Fire?";
+	question: "What type of creature did Harry have to fight in the Goblet of Fire? in the first trial";
 	answer: "dragon",
 	difficulty: 1, 
 	topic: "Hogwarts Level 1",
+},
+{ 
+	question: "How many siblings make up the Weasley family?",
+	answer: "7",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "Who was the Herboligy teacher?",
+	answer: "Professor Sprout",
+	difficulty 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{	
+	question: "Who resides in the first floor girls bathroom?",
+	answer: "Moaning Myrtle",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "What is another name for Felix Felicis?",
+	answer: "Liquid Luck",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "Who did Harry Potter take to the Slughorn Christmas party?",
+	answer: "Luna Lovegood",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question; "What is Voldemort's real name?",
+	answer: "Tom Marvolo Riddle",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "What type of creature lives off the souls of others?",
+	answer: "Death Eaters",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "What is the name of the famous wandmaker that the characters receive all their wands from?",
+	answer: "Garrick Ollivander",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "What House was Horace Slughorn in?",
+	answer: "Slytherin",
+	difficulty: 2, 
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "Who died in the Triwizard Maze?",
+	answer: "Cedric Diggory", 
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+	
+},
+{
+	question: "How many books are in the original Harry Potter series?",
+	answer: "7",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "How many siblings does Harry Potter have?",
+	answer: "0",
+	difficulty: 2,
+	topic: "Chamber of Secrets Level 2",
+},
+{
+	question: "Who guards the entrance to the Gryffindor commons?",
+	answer: "The Fat Lady",
+	difficlty: 3,
+	topic: "Azkaban Level 3",
+},
+{
+	question: "What does O.W.L. stand for?",
+	answer: "Ordinary Wizarding Level",
+	difficulty: 3,
+	topic: "Azkaban Level 3",
+},
+{
+	question: "What is the term used to describe a wizard/witch that cannot do magic?",
+	answer: "Squib",
+	difficulty: 3, 
+	topic: "Azkaban Level 3",
+},
+{
+	question: "Who kills Sirius Black?",
+	answer: "Bellatrix Lestrange",
+	difficulty: 3, 
+	topic: "Azkaban Level 3",
+},
+{
+	question: "What does one say to close the Marauder's Map and make it blank?",
+	answer: "Mischief Managed",
+	difficulty: 3,
+	topic: "Azkaban Level 3",
+},
+{
+	question: "What is the name of Filch's cat?"
+	answer: "Mrs. Norris",
+	difficulty: 3,
+	topic: "Azkaban Level 3", 
+},
+{
+	question: "What professor teaches flying lessons?",
+	answer: "Madam Hooch",
+	difficulty: 3, 
+	topic: "Azkaban Level 3",
+},
+{
+	question: "What is the model of the first broom Harry ever received?",
+	answer: "Nimbus 2000",
+	difficulty: 3,
+	topic: "Azkaban Level 3",
+},
+{
+	question: "What quidditch position does Harry Potter play?",
+	answer: "Seeker",
+	difficulty: 3,
+	topic: "Azkaban Level 3",
+},
+{
+	question: "How do you summon a Patronus",
+	answer: "Expecto Patronum",
+	difficulty: 3,
+	topic: "Azkaban Level 3", 
+},
+{
+	question: "What House is Nearly Headless Nick the ghost for?",
+	answer: "Gryffindor",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4",
+},
+{
+	question: "How many Death Eaters fought at the Battle of Hogwarts?",
+	answer: "23",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4",
+},
+{
+	question: "What does Sirius Black give Harry Ptter as a gift?",
+	answer: "Firebolt",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4",
+},
+{
+	question: "What do Harry, Hermione, and Ron drink to transform themselves into other people?",
+	answer: "Polyjuice Potion",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4",
+},
+{
+	question: "Which Hogwarts Founder did the Sorting Hat originally belong to?",
+	answer: "Godrick Gryffindor",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4",
+},
+{
+	question: "What is the name of the dragon that Hagrid hatches from an egg?",
+	answer: "Norbert",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4", 
+},
+{
+	question: "Who owned the Invisibility Cloak before Harry Potter?",
+	answer: "James Potter",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4",
+},
+{
+	question: "Name the magical creature that can turn itself into a person's worst fear.",
+	answer: "Boggart",
+	difficulty: 4,
+	topic: "The Triwizard Maze Level 4",
+},
+{
+	question: "What guards Azkaban prison?",
+	answer: "Dementors",
+	difficulty: 5,
+	topic: "Ministry of Magic Level 5", 
+},
+{
+	question: "What did the Weasley's and Harry Potter use to get to Daigon Alley in the first Harry Potter book?",
+	answer: "Floo Powder",
+	difficulty: 5,
+	topic: "Ministry of Magic Level 5",
+},
+{
+	question: "What Patronus is Harry Potter?",
+	answer: "Stag",
+	difficulty: 5,
+	topic: "Ministry of Magic Level 5",
+},
+{
+	question: "What are Voldemort's followers called?",
+	answer: "Death Eaters",
+	difficulty: 5,
+	topic: "Ministry of Magic Level 5",
+},
+{
+	question: "What Wizarding World slur does Draco call Hermione?",
+	answer: "Mudblood",
+	difficulty: 5,
+	topic: "Ministry of Magic Level 5", 
+},
+{
+	question: "According to the book, what color are Harry's eyes?",
+	answer: "green",
+	difficulty: 5,
+	topic: "Ministry of Magic Level 5",
+},
+{
+	question: "Who is the Half-Blood-Prince?",
+	answer: "Snape",
+	difficulty: 6,
+	topic: "The Tower Level 6",
+},
+{
+	question: "What chess piece does Ron play in Wizards Chess in the Sorcerer's Stone?",
+	answer: "knight",
+	difficulty: 6, 
+	topic: "The Tower Level 6",
+},
+{
+	question: "What day and month is Harry Potter's birthday?",
+	answer: "July 31",
+	difficulty; 6,
+	topic: "The Tower Level 6",
+},
+{
+	question: "What does Snape try to teach Harry Potter at Dumbledore's request?",
+	answer: "Occlumency",
+	difficulty: 6,
+	topic: "The Tower Level 6",
 }
+];
+
+
+
+
+	
+	
+	
 
 
 
